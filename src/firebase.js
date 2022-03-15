38ea551e8680c52714229504377fad7db217b53f
Vinay <itsvinayadav@gmail.com>
2022-03-15 14:13:42 +0530

    Project Initialization + basic Cleanup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_YOUTUBE,
+    authDomain: "clone-b4559.firebaseapp.com",
+    projectId: "clone-b4559",
+    storageBucket: "clone-b4559.appspot.com",
+    messagingSenderId: "802441476392",
+    appId: "1:802441476392:web:64f4fab851eda9a1c1781d",
+    measurementId: "G-5YJ349QPNF"
+};
