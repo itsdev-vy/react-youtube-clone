e6013cd3bd7e4ac5fda1bf041ee1104d78626edc
Vinay <itsvinayadav@gmail.com>
2022-03-16 16:03:04 +0530

    Done with Header and made it sticky

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Let Make Youtube Clone 🚀</h2>
+    <div className="app">
+      <Header />
+      <div className='app__page'>
+        <Sidebar />
+        {/* <RecommendedVideo /> */}
+      </div>
     </div>
   );
 }
