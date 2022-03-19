3f55276c0e012dc0df2411348d2134828c9d9a4b
Vinay <itsvinayadav@gmail.com>
2022-03-19 15:55:28 +0530

    Done with HomeScreen Recommended UI part

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,78 @@
 import React from 'react';
 import './RecommendedVideos.css';
+import VideoCard from './VideoCard';
 
 const RecommendedVideos = () => {
     return (
         <div className='recommendedVideos'>
-            <h2>RecommendedVideos</h2>
+            <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard
+                    title="Sasta Shaark Tank | Ashish Chanchlani"
+                    views="2.1 crore views"
+                    timestamp="3 days ago"
+                    channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+                    channel="ashish chanchlani vines"
+                    image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                />
+                <VideoCard
+                    title="Heropanti 2 - Official Trailer | Tiger S Tara S Nawazuddin | Sajid Nadiadwala |Ahmed Khan|29th April"
+                    views="5.1 crore views"
+                    timestamp="2 days ago"
+                    channelImage=" https://yt3.ggpht.com/ytc/AKedOLRHdXkc2SIOXv_QuyzDT_ECYAEStOHgzwoUqGMmcQc=s88-c-k-c0x00ffffff-no-rj"
+                    channel="NadiadwalaGrandson"
+                    image="https://i.ytimg.com/vi/Faz9PrY6xF8/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBtFH00YsymmwWhVapr8yuxUfiShQ"
+                />
+                <VideoCard
+                    title="Sasta Shaark Tank | Ashish Chanchlani"
+                    views="2.1 crore views"
+                    timestamp="3 days ago"
+                    channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+                    channel="ashish chanchlani vines"
+                    image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                />
+                <VideoCard
+                    title="Heropanti 2 - Official Trailer | Tiger S Tara S Nawazuddin | Sajid Nadiadwala |Ahmed Khan|29th April"
+                    views="5.1 crore views"
+                    timestamp="2 days ago"
+                    channelImage=" https://yt3.ggpht.com/ytc/AKedOLRHdXkc2SIOXv_QuyzDT_ECYAEStOHgzwoUqGMmcQc=s88-c-k-c0x00ffffff-no-rj"
+                    channel="NadiadwalaGrandson"
+                    image="https://i.ytimg.com/vi/Faz9PrY6xF8/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBtFH00YsymmwWhVapr8yuxUfiShQ"
+                />
+                <VideoCard
+                    title="Sasta Shaark Tank | Ashish Chanchlani"
+                    views="2.1 crore views"
+                    timestamp="3 days ago"
+                    channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+                    channel="ashish chanchlani vines"
+                    image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                />
+                <VideoCard
+                    title="Heropanti 2 - Official Trailer | Tiger S Tara S Nawazuddin | Sajid Nadiadwala |Ahmed Khan|29th April"
+                    views="5.1 crore views"
+                    timestamp="2 days ago"
+                    channelImage=" https://yt3.ggpht.com/ytc/AKedOLRHdXkc2SIOXv_QuyzDT_ECYAEStOHgzwoUqGMmcQc=s88-c-k-c0x00ffffff-no-rj"
+                    channel="NadiadwalaGrandson"
+                    image="https://i.ytimg.com/vi/Faz9PrY6xF8/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBtFH00YsymmwWhVapr8yuxUfiShQ"
+                />
+                <VideoCard
+                    title="Sasta Shaark Tank | Ashish Chanchlani"
+                    views="2.1 crore views"
+                    timestamp="3 days ago"
+                    channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+                    channel="ashish chanchlani vines"
+                    image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                />
+                <VideoCard
+                    title="Heropanti 2 - Official Trailer | Tiger S Tara S Nawazuddin | Sajid Nadiadwala |Ahmed Khan|29th April"
+                    views="5.1 crore views"
+                    timestamp="2 days ago"
+                    channelImage=" https://yt3.ggpht.com/ytc/AKedOLRHdXkc2SIOXv_QuyzDT_ECYAEStOHgzwoUqGMmcQc=s88-c-k-c0x00ffffff-no-rj"
+                    channel="NadiadwalaGrandson"
+                    image="https://i.ytimg.com/vi/Faz9PrY6xF8/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBtFH00YsymmwWhVapr8yuxUfiShQ"
+                />
+
+            </div>
         </div>
     )
 }
