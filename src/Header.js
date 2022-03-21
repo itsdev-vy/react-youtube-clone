c2f235da9f07e7524e0d6dc543cff9240b558a7c
Vinay <itsvinayadav@gmail.com>
2022-03-21 17:14:31 +0530

    Done with Search section UI and used Dummy data for all output .  also used react router dom for search page result ....no use of api all data displayed was dummy local.......COMPLETE!!!!!!

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import './Header.css';
 
 import MenuIcon from '@material-ui/icons/Menu';
@@ -7,25 +7,31 @@ import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Avatar from '@material-ui/core/Avatar';
-
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
+    const [inputSearch, setInputSearch] = useState('');
+
     return (
         <div className='header'>
 
             <div className='header__left'>
                 <MenuIcon />
-                <img
-                    className='header__logo'
-                    src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
-                    alt="YouTube"
-                />
+                <Link to='/'>
+                    <img
+                        className='header__logo'
+                        src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+                        alt="YouTube"
+                    />
+                </Link>
             </div>
 
             <div className='header__input'>
-                <input placeholder="Search" type="text" />
-                <SearchIcon className='header__inputButton' />
+                <input onChange={e => setInputSearch(e.target.value)} value={inputSearch} placeholder="Search" type="text" />
+                <Link to={`/search/${inputSearch}`}>
+                    <SearchIcon className='header__inputButton' />
+                </Link>
             </div>
 
             <div className='header__icons'>
---
+++ src/SearchPage.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import './SearchPage.css';
+import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
+import ChannelRow from './ChannelRow';
+import VideoRow from './VideoRow';
+
+const SearchPage = () => {
+    return (
+        <div className='searchPage'>
+            <div className='searchPage__filter'>
+                <TuneOutlinedIcon />
+                <h2>FILTER</h2>
+            </div>
+            <hr />
+            <ChannelRow
+                image='https://yt3.ggpht.com/ytc/AKedOLRbdv3Di8paQyrgMF_VwFXPkhwVzcW59Vgo8dTsyw=s176-c-k-c0x00ffffff-no-rj-mo'
+                channel="Clever Programmer"
+                verified
+                subs="660k"
+                noOfVideos={382}
+                description="You can find awesome programming lessons here! Also, expect programming  tips and tricks that will take your coding skills to the ..."
+            />
+            <hr />
+            <VideoRow
+                views="2.1 crore views"
+                subs="500k"
+                description="You can find Entertainment Videos..."
+                timestamp="3 days ago"
+                channel="ashish chanchlani vines"
+                title="Sasta Shaark Tank | Ashish Chanchlani"
+                image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+            />
+            <VideoRow
+                views="2.1 crore views"
+                subs="500k"
+                description="You can find Entertainment Videos..."
+                timestamp="3 days ago"
+                channel="ashish chanchlani vines"
+                title="Sasta Shaark Tank | Ashish Chanchlani"
+                image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+            />
+            <VideoRow
+                views="2.1 crore views"
+                subs="500k"
+                description="You can find Entertainment Videos..."
+                timestamp="3 days ago"
+                channel="ashish chanchlani vines"
+                title="Sasta Shaark Tank | Ashish Chanchlani"
+                image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+            />
+            <VideoRow
+                views="2.1 crore views"
+                subs="500k"
+                description="You can find Entertainment Videos..."
+                timestamp="3 days ago"
+                channel="ashish chanchlani vines"
+                title="Sasta Shaark Tank | Ashish Chanchlani"
+                image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+            />
+            <VideoRow
+                views="2.1 crore views"
+                subs="500k"
+                description="You can find Entertainment Videos..."
+                timestamp="3 days ago"
+                channel="ashish chanchlani vines"
+                title="Sasta Shaark Tank | Ashish Chanchlani"
+                image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+            />
+            <VideoRow
+                views="2.1 crore views"
+                subs="500k"
+                description="You can find Entertainment Videos..."
+                timestamp="3 days ago"
+                channel="ashish chanchlani vines"
+                title="Sasta Shaark Tank | Ashish Chanchlani"
+                image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+            />
+            <VideoRow
+                views="2.1 crore views"
+                subs="500k"
+                description="You can find Entertainment Videos..."
+                timestamp="3 days ago"
+                channel="ashish chanchlani vines"
+                title="Sasta Shaark Tank | Ashish Chanchlani"
+                image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+            />
+            <VideoRow
+                views="2.1 crore views"
+                subs="500k"
+                description="You can find Entertainment Videos..."
+                timestamp="3 days ago"
+                channel="ashish chanchlani vines"
+                title="Sasta Shaark Tank | Ashish Chanchlani"
+                image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+            />
+            <VideoRow
+                views="2.1 crore views"
+                subs="500k"
+                description="You can find Entertainment Videos..."
+                timestamp="3 days ago"
+                channel="ashish chanchlani vines"
+                title="Sasta Shaark Tank | Ashish Chanchlani"
+                image="https://i.ytimg.com/vi/xrR3xQNeB_Y/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARtjALjhVHD-hdw9eSACw2YOyTew"
+                channelImage=" https://yt3.ggpht.com/ytc/AKedOLTE_sosWegmfiw0z7ronCP7O6PbEL14kOR7mK1Q5A=s88-c-k-c0x00ffffff-no-rj"
+            />
+        </div>
+    )
+}
+
+export default SearchPage;
